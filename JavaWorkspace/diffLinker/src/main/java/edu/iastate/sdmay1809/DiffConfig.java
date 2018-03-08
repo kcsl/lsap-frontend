2a25251860aa349465f9348a67f060b25534c322
dhanwada <dhanwada@iastate.edu>
2018-03-07 23:05:04 -0600

    Add DiffConfig Object
    This commit adds a diff config object that can be used to configure
    the diff linker. This config object can be created and passed in the
    constructor to manage the various options of the diff linker.
    
    Eventually this object will also be available for the diff mapper
    since the two modules are very similar.

---
+++ JavaWorkspace/diffLinker/src/main/java/edu/iastate/sdmay1809/DiffConfig.java
@@ -0,0 +1,12 @@
+package edu.iastate.sdmay1809;
+
+public class DiffConfig {
+	public String OLD_TAG = "v3.17-rc1";
+	public String NEW_TAG = "v3.18-rc1";
+	public String DIFF_TEST_DIR = "/Volumes/dhanwada_cs/";
+	public String KERNEL_DIR = "/Volumes/dhanwada_cs/kernel/";
+	public String RESULT_DIR = "/Volumes/dhanwada_cs/sdmay18-09/linux-kernel-" + OLD_TAG.substring(1) + "/";
+	public String[] TYPES = { "mutex", "spin" };
+
+	
+}
