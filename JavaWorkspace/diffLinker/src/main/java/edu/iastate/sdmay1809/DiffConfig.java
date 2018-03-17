b79beebc5171d41674a0f30f6812bc9b8d27f59a
dhanwada <dhanwada@iastate.edu>
2018-03-17 01:47:55 -0500

    Add Config Builder from JSON file
    This commit adds an option to build a config based on an input
    json file. The config will try to parse the given file as a json
    object and if it fails will simply return a default config builder.
    
    If a json object is parsed, it will look for values based on given
    keys. If a null value is found for a key, the default value will be
    used by the config builder. If a non null value is found, that value
    will be used in place of the default when returning the builder
    
    This commit also changes the default values to use relative paths
    based on the current working directory. The directory structures
    matches a proposed structure of the project so ensure consistency.

--- JavaWorkspace/diffLinker/src/main/java/edu/iastate/sdmay1809/DiffConfig.java
+++ JavaWorkspace/diffLinker/src/main/java/edu/iastate/sdmay1809/DiffConfig.java
@@ -1,26 +1,45 @@
 package edu.iastate.sdmay1809;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.json.JSONObject;
+
 public class DiffConfig {
-	
-	public String OLD_TAG = "v3.17-rc1";
-	public String NEW_TAG = "v3.18-rc1";
-	public String DIFF_TEST_DIR = "/Volumes/dhanwada_cs/";
-	public String KERNEL_DIR = "/Volumes/dhanwada_cs/kernel/";
-	public String RESULT_DIR = "/Volumes/dhanwada_cs/sdmay18-09/linux-kernel-" + OLD_TAG.substring(1) + "/";
-	public String[] TYPES = { "mutex", "spin" };
-	
+
+	public String OLD_TAG;
+	public String NEW_TAG;
+	public String DIFF_TEST_DIR;
+	public String KERNEL_DIR;
+	public String RESULT_DIR;
+	public String[] TYPES;
+
 	public static class DiffConfigBuilder {
-		private String OLD_TAG = "v3.17-rc1";
-		private String NEW_TAG = "v3.18-rc1";
-		private String DIFF_TEST_DIR = "/Volumes/dhanwada_cs/";
-		private String KERNEL_DIR = "/Volumes/dhanwada_cs/kernel/";
-		private String RESULT_DIR = "/Volumes/dhanwada_cs/sdmay18-09/linux-kernel-" + OLD_TAG.substring(1) + "/";
-		private String[] TYPES = { "mutex", "spin" };
-		
+		private String OLD_TAG;
+		private String NEW_TAG;
+		private String DIFF_TEST_DIR;
+		private String KERNEL_DIR;
+		private String RESULT_DIR;
+		private String[] TYPES;
+
 		DiffConfigBuilder() {
-			// default constructor
+			String cwd = System.getProperty("user.dir");
+			if (!cwd.endsWith("/")) {
+				cwd = cwd + "/";
+			}
+			this.OLD_TAG = "v3.17-rc1";
+			this.NEW_TAG = "v3.18-rc1";
+			this.DIFF_TEST_DIR = cwd + "diffmap/";
+			this.KERNEL_DIR = cwd + "kernel/";
+			this.RESULT_DIR = cwd + "diffmap/prev_result/";
+			this.TYPES = new String[] { "mutex", "spin" };
 		}
-		
+
 		DiffConfigBuilder(DiffConfigBuilder base) {
 			this.OLD_TAG = base.OLD_TAG;
 			this.NEW_TAG = base.NEW_TAG;
@@ -29,42 +48,42 @@ public class DiffConfig {
 			this.RESULT_DIR = base.RESULT_DIR;
 			this.TYPES = base.TYPES;
 		}
-		
+
 		public DiffConfig build() {
 			return new DiffConfig(this);
 		}
-		
+
 		public DiffConfigBuilder setOldTag(String old_tag) {
 			this.OLD_TAG = Utils.coalesce(old_tag, OLD_TAG);
 			return this;
 		}
-		
+
 		public DiffConfigBuilder setNewTag(String new_tag) {
 			this.NEW_TAG = Utils.coalesce(new_tag, NEW_TAG);
 			return this;
 		}
-		
+
 		public DiffConfigBuilder setDiffTestDir(String diff_test_dir) {
 			this.DIFF_TEST_DIR = Utils.coalesce(diff_test_dir, DIFF_TEST_DIR);
 			return this;
 		}
-		
+
 		public DiffConfigBuilder setKernelDir(String kernel_dir) {
 			this.KERNEL_DIR = Utils.coalesce(kernel_dir, KERNEL_DIR);
 			return this;
 		}
-		
+
 		public DiffConfigBuilder setResultDir(String result_dir) {
 			this.RESULT_DIR = Utils.coalesce(result_dir, RESULT_DIR);
 			return this;
 		}
-		
+
 		public DiffConfigBuilder setTypes(String[] types) {
 			this.TYPES = Utils.coalesce(types, TYPES);
 			return this;
 		}
 	}
-	
+
 	private DiffConfig(DiffConfigBuilder builder) {
 		this.OLD_TAG = builder.OLD_TAG;
 		this.NEW_TAG = builder.NEW_TAG;
@@ -73,12 +92,62 @@ public class DiffConfig {
 		this.RESULT_DIR = builder.RESULT_DIR;
 		this.TYPES = builder.TYPES;
 	}
-	
+
 	public static DiffConfigBuilder builder() {
 		return new DiffConfigBuilder();
 	}
-	
+
 	public static DiffConfigBuilder builder(DiffConfigBuilder base) {
 		return new DiffConfigBuilder(base);
+	}
+
+	public static DiffConfigBuilder builder(String fileName) {
+		Charset utf8;
+		Path configFilePath;
+		String cwd;
+		String configFileContent;
+		String[] keys = { "old_tag", "new_tag", "diff_test_dir", "kernel_dir", "result_dir", "types" };
+		String[] values = new String[5];
+		String[] types_val;
+
+		fileName = Utils.coalesce(fileName, "config.json");
+		cwd = System.getProperty("user.dir");
+		configFilePath = Paths.get(cwd, "/", fileName);
+		utf8 = Charset.forName("UTF-8");
+		configFileContent = "{}";
+		try {
+			configFileContent = String.join("\n", Files.readAllLines(configFilePath, utf8));
+		} catch (IOException e) {
+			System.err.println("WARN: DiffConfig file could not be read, using default values");
+			return new DiffConfigBuilder();
+		}
+
+		JSONObject configObject = new JSONObject(configFileContent);
+
+		// Loop through all the String values and get them from their keys
+		// If an exception is thrown reading the string, set it to null so the
+		// config builder will use the default value.
+		for (int i = 0; i < values.length; i++) {
+			try {
+				values[i] = configObject.getString(keys[i]);
+			} catch (Exception e) {
+				values[i] = null;
+			}
+		}
+
+		try {
+			// Get a json array for the "types" key, convert it to a stream to
+			// perform
+			// String conversion and collect the values to a list, then convert
+			// the list to an array
+			types_val = (String[]) configObject.getJSONArray(keys[5]).toList().stream()
+					.map(object -> Objects.toString(object, null)).collect(Collectors.toList()).toArray();
+		} catch (Exception e) {
+			types_val = null;
+		}
+
+		// Return a DiffConfigBuilder using the values from the config file.
+		return new DiffConfigBuilder().setOldTag(values[0]).setNewTag(values[1]).setDiffTestDir(values[2])
+				.setKernelDir(values[3]).setResultDir(values[4]).setTypes(types_val);
 	}
 }
