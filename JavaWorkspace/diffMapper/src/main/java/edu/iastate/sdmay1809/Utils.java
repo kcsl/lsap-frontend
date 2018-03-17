384a360c309afbf50b66ba98295259a7752c0164
dhanwada <dhanwada@iastate.edu>
2018-03-17 11:26:40 -0500

    Move Shared Files to DiffMapper
    This commit moves the shared DiffConfig and Utils files over from the
    DiffLinker project to the DiffMapper project. This is a temporary
    measure to easily get the definitions to this project. Eventually,
    it would be better to break out the shared codebase into a separate
    project so it can be developed independently.

--- JavaWorkspace/diffMapper/src/main/java/edu/iastate/sdmay1809/Utils.java
+++ JavaWorkspace/diffMapper/src/main/java/edu/iastate/sdmay1809/Utils.java
@@ -4,15 +4,43 @@ import java.lang.reflect.Array;
 
 public class Utils {
 
-	public static <T> T[] concatenate (T[] a, T[] b) {
-	    int aLen = a.length;
-	    int bLen = b.length;
+	@SafeVarargs
+	public static <T> T coalesce(T... params) {
+		if (params == null) {
+			return null;
+		}
+		for (T param : params) {
+			if (param != null) {
+				return param;
+			}
+		}
+		return null;
+	}
 
-	    @SuppressWarnings("unchecked")
-	    T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen+bLen);
-	    System.arraycopy(a, 0, c, 0, aLen);
-	    System.arraycopy(b, 0, c, aLen, bLen);
+	public static <T> T[] concatenate(T[] a, T[] b) {
+		if (a == null && b == null) {
+			return null;
+		}
 
-	    return c;
+		int aLen = (a != null) ? a.length : 0;
+		int bLen = (b != null) ? b.length : 0;
+		
+		if (a == null) {
+			@SuppressWarnings("unchecked")
+			T[] c = (T[]) Array.newInstance(b.getClass().getComponentType(), aLen + bLen);
+			System.arraycopy(b, 0, c, 0, bLen);
+			return c;
+		} else if (b == null) {
+			@SuppressWarnings("unchecked")
+			T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
+			System.arraycopy(a, 0, c, 0, aLen);
+			return c;
+		} else {
+			@SuppressWarnings("unchecked")
+			T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
+			System.arraycopy(a, 0, c, 0, aLen);
+			System.arraycopy(b, 0, c, aLen, bLen);
+			return c;
+		}
 	}
 }
