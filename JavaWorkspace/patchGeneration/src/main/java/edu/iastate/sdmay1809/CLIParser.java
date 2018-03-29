0af906deeb720de8d954cc47fcb479825063ccb1
Collin McIntyre <crm11396@gmail.com>
2018-03-29 15:48:37 -0500

    Finished parser implementation and added tests

--- JavaWorkspace/patchGeneration/src/main/java/edu/iastate/sdmay1809/CLIParser.java
+++ JavaWorkspace/patchGeneration/src/main/java/edu/iastate/sdmay1809/CLIParser.java
@@ -3,6 +3,7 @@ package edu.iastate.sdmay1809;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -20,42 +21,51 @@ public class CLIParser {
 		options.addOption("v", "verbose", false, "Print additional information to the console");
 		options.addOption(Option.builder("kp")
 								.longOpt("kernel-path")
-								.hasArg()
-								.valueSeparator(' ')
-								.desc("The path to the root directory of the kernel being patched. If not present, the path will defualt to the current directory.")
+								.optionalArg(true)
+								.numberOfArgs(1)
+								.argName("path")
+								.desc("The path to the root directory of the kernel being patched. By default, it is the current directory (\".\")")
 								.build());
 		options.addOption(Option.builder("d")
 								.longOpt("debug")
-								.hasArg()
-								.valueSeparator(' ')
-								.desc("If present, the Patcher will write the content of the patched header files to text files. These text files will be found in the location provided, or in the current directory if no directory is provided.")
+								.optionalArg(true)
+								.numberOfArgs(1)
+								.argName("path")
+								.desc("The file in which to store the patched header file output. By default, it is \"./PatcherDebug.txt\"")
 								.build());
+		options.addOption("h", "help", false, "Prints additional help");
 		
 		line = parser.parse(options, args);
+		
+		if (line.hasOption('h'))
+		{
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("patcher", "Options available", options, "", true);
+		}
 	}
 	
 	public boolean vProvided()
 	{
-		return line.hasOption('v') || line.hasOption("verbose");
+		return line.hasOption('v');
 	}
 	
 	public boolean kpProvided()
 	{
-		return line.hasOption("kp") || line.hasOption("kernel-path");
+		return line.hasOption("kp");
 	}
 	
 	public boolean dProvided()
 	{
-		return line.hasOption('d') || line.hasOption("debug");
+		return line.hasOption('d');
 	}
 	
-	public String getKpVal()
+	public String getKPVal()
 	{
-		return (kpProvided()) ? line.getOptionValue("kernel-path") : "";
+		return line.getOptionValue("kernel-path", "");
 	}
 	
 	public String getDVal()
 	{
-		return (dProvided()) ? line.getOptionValue("debug") : "";
+		return line.getOptionValue("debug", "PatcherDebug.txt");
 	}
 }
---
+++ JavaWorkspace/patchGeneration/src/test/java/edu/iastate/sdmay1809/CLIParserTest.java
@@ -0,0 +1,141 @@
+package edu.iastate.sdmay1809;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.apache.commons.cli.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CLIParserTest {
+	CLIParser parser;
+	ByteArrayOutputStream outContent;
+	
+	@Before
+	public void setUpStream()
+	{
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	}
+	
+	@After
+	public void restoreStream()
+	{
+		System.setOut(System.out);
+	}
+	
+	@Test
+	public void cliParserConstructor() throws ParseException
+	{
+		String[] args = {""};
+		parser = new CLIParser(args);
+		assertThat(parser, instanceOf(CLIParser.class));
+	}
+	
+	@Test
+	public void cliParserVProvided() throws ParseException
+	{
+		String[] args = {"-v"};
+		parser = new CLIParser(args);
+		assertTrue(parser.vProvided());
+
+		args[0] = "--verbose";
+		parser = new CLIParser(args);
+		assertTrue(parser.vProvided());
+	}
+	
+	@Test
+	public void cliParserDProvided() throws ParseException
+	{
+		String[] args = {"-d", "path"};
+		parser = new CLIParser(args);
+		assertTrue(parser.dProvided());
+
+		args[0] = "--debug";
+		parser = new CLIParser(args);
+		assertTrue(parser.dProvided());
+	}
+	
+	@Test
+	public void cliParserPProvided() throws ParseException
+	{
+		String[] args = {"-kp", "path"};
+		parser = new CLIParser(args);
+		assertTrue(parser.kpProvided());
+
+		args[0] = "--kernel-path";
+		parser = new CLIParser(args);
+		assertTrue(parser.kpProvided());
+	}
+	
+	@Test
+	public void cliParserAllArgsProvided() throws ParseException
+	{
+		String[] args = {"--verbose", "-kp", "path", "--debug", "path"};
+		parser = new CLIParser(args);
+		assertTrue(parser.vProvided());
+		assertTrue(parser.kpProvided());
+		assertTrue(parser.dProvided());
+	}
+	
+	@Test
+	public void cliParserGetKPVal() throws ParseException
+	{
+		String[] args = {"-kp", "/path/to/kernel"};
+		parser = new CLIParser(args);
+		assertTrue(parser.kpProvided());
+		assertEquals(parser.getKPVal(), "/path/to/kernel");
+
+		args[0] = "--kernel-path";
+		parser = new CLIParser(args);
+		assertTrue(parser.kpProvided());
+		assertEquals(parser.getKPVal(), "/path/to/kernel");
+	}
+	
+	@Test
+	public void cliParserGetDVal() throws ParseException
+	{
+		String[] args = {"-d", "/path/to/debug.txt"};
+		parser = new CLIParser(args);
+		assertTrue(parser.dProvided());
+		assertEquals(parser.getDVal(), "/path/to/debug.txt");
+
+		args[0] = "--debug";
+		parser = new CLIParser(args);
+		assertTrue(parser.dProvided());
+		assertEquals(parser.getDVal(), "/path/to/debug.txt");
+	}
+	
+	@Test
+	public void cliParserDefaultVals() throws ParseException
+	{
+		String[] args = {"-d", "-kp"};
+		parser = new CLIParser(args);
+		assertTrue(parser.dProvided());
+		assertTrue(parser.kpProvided());
+		assertEquals(parser.getDVal(), "PatcherDebug.txt");
+		assertEquals(parser.getKPVal(), "");
+	}	
+	
+	@Test
+	public void cliParserHelpMenu() throws ParseException
+	{		
+		String[] args = {"-h"};
+		parser = new CLIParser(args);
+		assertTrue(outContent.toString().length() > 0);
+		
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		assertEquals(outContent.toString().length(), 0);
+		
+		args[0] = "--help";
+		parser = new CLIParser(args);
+		assertTrue(outContent.toString().length() > 0);
+	}
+}
