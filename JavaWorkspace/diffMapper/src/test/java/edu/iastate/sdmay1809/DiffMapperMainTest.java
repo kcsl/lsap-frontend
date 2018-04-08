ee9970ac8e48c3e5500cbf4eb2bf36d14e8a65ba
dhanwada <dhanwada@iastate.edu>
2018-04-08 04:11:08 -0500

    Add Test for DiffMapper Main method
    This commit adds a test to check if the DiffMapper main method
    completes successfully, The test uses the same settings as the
    DiffMapper single instance test, so we only have to check to make sure
    the method completes successfully.

---
+++ JavaWorkspace/diffMapper/src/test/java/edu/iastate/sdmay1809/DiffMapperMainTest.java
@@ -0,0 +1,57 @@
+package edu.iastate.sdmay1809;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.json.JSONException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.rules.TemporaryFolder;
+
+public class DiffMapperMainTest {
+	@Rule
+	public TemporaryFolder testFolder = new TemporaryFolder();
+	
+	@Rule
+	public SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	@Test
+	public void testInit() {
+		DiffMapperMain dmm = new DiffMapperMain();
+		
+		assertNotNull(dmm);
+		assertThat(dmm, instanceOf(DiffMapperMain.class));
+	}
+	
+	@Test
+	public void runMain() throws JSONException, IOException, InterruptedException, Exception {
+		// Setup Test Workspace!
+		File workspace = testFolder.newFolder();
+		File baseDir = Paths.get("resources", "testing", "DiffMapper", "runSingleInstance").toAbsolutePath().toFile();
+		Utils.execute(new String[] { "cp", "-R", ".", workspace.getAbsolutePath() }, baseDir);
+
+		// Git Setup!
+		Utils.execute(new String[] { "cp", "-R", ".notgit/", "kernel/.git/" }, workspace);
+		
+		String[] args = new String[] {
+				"-Ddiff_test_dir=" + Paths.get(workspace.getAbsolutePath(), "diffmap/").toString(),
+				"-Dkernel_dir=" + Paths.get(workspace.getAbsolutePath(), "kernel/").toString(),
+				"-Dresult_dir=" + Paths.get(workspace.getAbsolutePath(), "diffmap", "results").toString(),
+				"-Dold_tag=v3.19-rc1",
+				"-Dnew_tag=v3.20",
+				"-Dtypes.1=mutex"
+		};
+		
+		DiffMapperMain.main(args);
+		
+		assertThat(systemOutRule.getLog(), containsString("Done! Applied 1 changes"));
+		assertThat(systemOutRule.getLog(), not(containsString("[ERROR] could not map differences!")));
+	}
+}
