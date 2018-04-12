3f85637255c59ede54630d11debe7a4018524e7a
dhanwada <dhanwada@iastate.edu>
2018-04-12 02:02:28 -0500

    Optimize DiffLinker
    This commit aims to optimize the DiffLinker by first structuring instances
    by filename instead of randomly. This allows a file to remain open for
    all instances in it before closing. Rather than opening and closing the
    same file multiple times.

--- JavaWorkspace/diffLinker/src/main/java/edu/iastate/sdmay1809/DiffLinker.java
+++ JavaWorkspace/diffLinker/src/main/java/edu/iastate/sdmay1809/DiffLinker.java
@@ -1,5 +1,6 @@
 package edu.iastate.sdmay1809;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
@@ -8,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -64,16 +66,20 @@ public class DiffLinker {
 		Path newInstance = Paths.get(config.DIFF_TEST_DIR, newMapFilename);
 		String newInstanceContent = String.join("\n", Files.readAllLines(newInstance, utf8));
 		JSONArray newMap = new JSONArray(newInstanceContent);
-
+		JSONObject newMapOptimized = new JSONObject();
 		JSONArray mapping = new JSONArray();
 
-		println("Linking instances...");
-		for (int i = 0; i < newMap.length(); i++) {
-			JSONObject instance = newMap.getJSONObject(i);
+		optimizeMapStructure(newMap, newMapOptimized);
 
-			if (linkInstances(mapping, instance)) {
-				instancesLinked++;
-			}
+		println("Linking instances...");
+
+		Iterator<String> iter = newMapOptimized.keys();
+
+		while (iter.hasNext()) {
+			String key = iter.next();
+			JSONArray instances = newMapOptimized.getJSONArray(key);
+
+			instancesLinked += linkInstances(key, instances, mapping);
 		}
 
 		mapping.write(new PrintWriter(Paths.get(config.DIFF_TEST_DIR, "diffInstanceMap.json").toFile()), 1, 2);
@@ -81,26 +87,62 @@ public class DiffLinker {
 		return instancesLinked;
 	}
 
-	public boolean linkInstances(JSONArray mapping, JSONObject object) throws IOException {
-		String filename = object.getString("filename");
-		long offset = object.getLong("offset");
+	private void optimizeMapStructure(JSONArray map, JSONObject optimized) {
+		for (int i = 0; i < map.length(); i++) {
+			JSONObject instance = map.getJSONObject(i);
 
-		String name = object.getString("name");
-		String status = object.getString("status");
-		String id = object.getString("id");
-		long length = object.getLong("length");
+			String filename = instance.getString("filename");
 
-		String metadata = null;
+			JSONArray fileInstances = null;
+			if (optimized.has(filename)) {
+				fileInstances = optimized.getJSONArray(filename);
+			} else {
+				fileInstances = new JSONArray();
+				optimized.put(filename, fileInstances);
+			}
+
+			fileInstances.put(instance);
+		}
+	}
+
+	private int linkInstances(String filename, JSONArray instances, JSONArray diffMap) throws IOException {
+		JSONArray sorted = sort(instances);
+		int length = sorted.length();
+		int instancesLinked = 0;
+
 		Path fileToSearch = Paths.get(config.KERNEL_DIR, filename);
-
 		println("Searching through: " + fileToSearch.toString());
 		// Need to capture comment data within a certain threshold of lines
 
+		RandomAccessFile r = new RandomAccessFile(fileToSearch.toFile(), "rw");
+
+		for (int i = 0; i < length; i++) {
+			JSONObject instance = sorted.getJSONObject(i);
+			
+			if(linkInstance(instance, diffMap, r)) {
+				instancesLinked++;
+			}
+
+		}
+
+		r.close();
+
+		return instancesLinked;
+	}
+
+	private boolean linkInstance(JSONObject instance, JSONArray diffMap, RandomAccessFile r) throws IOException {
+		long offset = instance.getLong("offset");
+		String filename = instance.getString("filename");
+		String name = instance.getString("name");
+		String status = instance.getString("status");
+		String id = instance.getString("id");
+		long length = instance.getLong("length");
+
+		String metadata = null;
 		String[] linesFound = {};
 		String buffer = "";
 		byte[] byteBuffer = new byte[40]; // byte buffer to capture data 40
 											// bytes at a time
-		RandomAccessFile r = new RandomAccessFile(fileToSearch.toFile(), "rw");
 
 		// Start at offset
 		r.seek(offset);
@@ -142,11 +184,8 @@ public class DiffLinker {
 				break;
 			}
 		}
-
-		// Close the file
-		r.close();
-
-		println("Searching through " + linesFound.length + " lines");
+		
+		println("Found " + linesFound.length + " lines");
 
 		// Search for our string
 		for (int i = linesFound.length - 1; i >= 0; i--) {
@@ -179,14 +218,39 @@ public class DiffLinker {
 			map.put("new", newData);
 		}
 
-		mapping.put(map);
+		diffMap.put(map);
 
 		return (metadata != null);
 	}
 
-	public void println(String msg) {
+	private void println(String msg) {
 		if (allowPrintStatements) {
 			System.out.println(msg);
 		}
+	}
+
+	private JSONArray sort(JSONArray array) throws JSONException {
+		JSONArray sorted = new JSONArray();
+
+		while (array.length() > 0) {
+			int minIdx = 0;
+			JSONObject min = array.getJSONObject(0);
+			JSONObject tmp;
+			for (int i = 1; i < array.length(); i++) {
+				tmp = array.getJSONObject(i);
+				if (tmp.getInt("offset") < min.getInt("offset")) {
+					min = tmp;
+					minIdx = i;
+				} else if (tmp.getInt("offset") == min.getInt("offset")
+						&& tmp.getString("id").equals(min.getString("id"))) {
+					// Duplicate detected -- remove it
+					array.remove(i);
+					i--;
+				}
+			}
+			sorted.put(array.remove(minIdx));
+		}
+
+		return sorted;
 	}
 }
